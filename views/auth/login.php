179363ef27b43e7afc932d6cc1841faa7c705eaa
colidom <colidom@outlook.com>
2023-02-21 22:31:45 +0000

    Creando la vista de Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,22 @@
-<h1>Login</h1>
+<main class="auth">
+    <h2 class="auth__header"><?php echo $titulo ?></h2>
+    <p class="auth__texto">Inicia sesión en DevWebcamp</p>
+
+    <form action="" class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input type="email" class="formulario_input" placeholder="Tu Email" id="email">
+        </div>
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Contraseña</label>
+            <input type="password" class="formulario_input" placeholder="Tu Contraseña" id="password">
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+    </form>
+
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? Registrarse</a>
+        <a href="/olvide" class="acciones__enlace">¿Olivaste tu contraseña? Recuperarla</a>
+    </div>
+</main>
