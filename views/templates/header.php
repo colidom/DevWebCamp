9b7164ad805161b9c991a3dc22e4a00c38f5686a
colidom <colidom@outlook.com>
2023-02-26 18:54:07 +0000

    Añadiendo el HTML del Header

---
+++ views/templates/header.php
@@ -0,0 +1,21 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="" header__registro>Registrarse</a>
+            <a href="/login" class="" header__registro>Iniciar sesión</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">
+                    &#60;DebWebcamp />
+                </h1>
+
+                <p class="header__texto">Octubre 5-6-2023</p>
+                <p class="header__texto header__texto--modalidad">En línea - Presencial</p>
+
+                <a href="/registro" class="header__boton">Comprar entradas</a>
+            </a>
+        </div>
+    </div>
+</header>
