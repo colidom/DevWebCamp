a09724bca1dab46fe9575a0580062e108c5c1418
colidom <colidom@outlook.com>
2023-02-28 00:57:51 +0000

    Creando la Tabla de Usuarios y Validación

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -2,18 +2,23 @@
     <h2 class="auth__header"><?php echo $titulo ?></h2>
     <p class="auth__texto">Registrarse en DevWebcamp</p>
 
-    <form action="" class="formulario">
+
+    <?php
+    require_once __DIR__ . '/../templates/alertas.php';
+    ?>
+
+    <form method="POST" action="/registro" class="formulario">
         <div class="formulario__campo">
             <label for="nombre" class="formulario__label">Nombre</label>
-            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre">
+            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre" name="nombre" value="<?php echo $usuario->nombre ?>">
         </div>
         <div class="formulario__campo">
             <label for="apellido" class="formulario__label">Apellido</label>
-            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido">
+            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido" name="apellido" value="<?php echo $usuario->apellido ?>">
         </div>
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
-            <input type="email" class="formulario__input" placeholder="Tu Email" id="email">
+            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email" value="<?php echo $usuario->email ?>">
         </div>
         <div class="formulario__campo">
             <label for="password" class="formulario__label">Contraseña</label>
