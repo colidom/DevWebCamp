e07881c45546b802e0b53409bc914ec3775beacb
colidom <colidom@outlook.com>
2023-03-02 00:51:53 +0000

    Añadida  tabla de categorías

---
+++ sql/categorias.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `categorias` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
